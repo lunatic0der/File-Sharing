096ea7ffdeb4836c0627e27026ebb51c520466de
mrismanaziz <mrismanaziz@gmail.com>
2022-02-13 23:06:04 +0700

    File-Sharing-Man

---
+++ config.py
@@ -0,0 +1,93 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+
+# Bot token @Botfather
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+
+# Your API ID from my.telegram.org
+APP_ID = int(os.environ.get("APP_ID", ""))
+
+# Your API Hash from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "")
+
+# Your db channel Id
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+
+# OWNER ID
+OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+
+# NAMA OWNER
+OWNER = os.environ.get("OWNER", "mrismanaziz")
+
+# CHANNEL & GROUP
+CHANNEL1 = os.environ.get("CHANNEL1", "Lunatic0de")
+CHANNEL2 = os.environ.get("CHANNEL2", "Lunatic0de")
+CHANNEL3 = os.environ.get("CHANNEL3", "SharingUserbot")
+CHANNEL4 = os.environ.get("CHANNEL4", "SharingUserbot")
+
+# Database
+DB_URI = os.environ.get("DATABASE_URL", "")
+
+# force sub channel id, if you want enable force sub
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "0"))
+FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "0"))
+FORCE_SUB_CHANNEL4 = int(os.environ.get("FORCE_SUB_CHANNEL4", "0"))
+
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+
+# start message
+START_MSG = os.environ.get(
+    "START_MESSAGE",
+    "<b>Hello {first}\n\nSaya dapat menyimpan file pribadi di Saluran Tertentu dan pengguna lain dapat mengaksesnya dari tautan khusus.</b>",
+)
+try:
+    ADMINS = []
+    for x in os.environ.get("ADMINS", "").split():
+        ADMINS.append(int(x))
+except ValueError:
+    raise Exception("Daftar Admin anda tidak berisi User ID yang valid.")
+try:
+    ADMINS = []
+    for x in os.environ.get("ADMINS", "").split():
+        ADMINS.append(int(x))
+except ValueError:
+    raise Exception("Daftar Admin anda tidak berisi User ID yang valid.")
+
+# Force sub message
+FORCE_MSG = os.environ.get(
+    "FORCE_SUB_MESSAGE",
+    "<b>Hello {first}\n\nAnda harus bergabung di Channel/Grup saya Terlebih dahulu untuk Melihat File yang saya Bagikan\n\nSilakan Join Terlebih Dahulu</b>",
+)
+
+# set your Custom Caption here, Keep None for Disable Custom Caption
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
+
+# Set true if you want Disable your Channel Posts Share button
+if os.environ.get("DISABLE_CHANNEL_BUTTON", None) == "True":
+    DISABLE_CHANNEL_BUTTON = True
+else:
+    DISABLE_CHANNEL_BUTTON = False
+
+ADMINS.append(OWNER_ID)
+ADMINS.append(844432220)
+ADMINS.append(1250450587)
+ADMINS.append(1750080384)
+ADMINS.append(2102118281)
+
+LOG_FILE_NAME = "logs.txt"
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)s] - %(name)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[
+        RotatingFileHandler(LOG_FILE_NAME, maxBytes=50000000, backupCount=10),
+        logging.StreamHandler(),
+    ],
+)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
---
+++ plugins/cbb.py
@@ -0,0 +1,30 @@
+# (©)Codexbotz
+# Recode By @mrismanaziz
+
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+
+from bot import Bot
+from config import CHANNEL1, CHANNEL2, CHANNEL3, CHANNEL4, OWNER
+
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "about":
+        await query.message.edit_text(
+            text=f"• Owner: @{OWNER}\n• Channel 1: @{CHANNEL1}\n• Channel 2: @{CHANNEL2}\n• Channel 3: @{CHANNEL3}\n•Channel 4: @{CHANNEL4}\nㅤ",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("ᴛᴜᴛᴜᴘ", callback_data="close"),
+                    ]
+                ]
+            ),
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
