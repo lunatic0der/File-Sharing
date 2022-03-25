9541b3e0e5d57efb701804a13e803b7675875194
mrismanaziz <mrismanaziz@gmail.com>
2022-03-26 03:08:09 +0700

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -12,37 +12,37 @@ from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 
 from config import (
     ADMINS,
-    FORCE_SUB_CHANNEL,
+    FORCE_SUB_CHANNEL1,
     FORCE_SUB_CHANNEL2,
-    FORCE_SUB_CHANNEL3,
-    FORCE_SUB_CHANNEL4,
+    FORCE_SUB_GROUP1,
+    FORCE_SUB_GROUP2,
 )
 
 
 async def is_subscribed(filter, client, update):
-    if not FORCE_SUB_CHANNEL:
+    if not FORCE_SUB_CHANNEL1:
         return True
     if not FORCE_SUB_CHANNEL2:
         return True
-    if not FORCE_SUB_CHANNEL3:
+    if not FORCE_SUB_GROUP1:
         return True
-    if not FORCE_SUB_CHANNEL4:
+    if not FORCE_SUB_GROUP2:
         return True
     user_id = update.from_user.id
     if user_id in ADMINS:
         return True
     try:
         member = await client.get_chat_member(
-            chat_id=FORCE_SUB_CHANNEL, user_id=user_id
+            chat_id=FORCE_SUB_CHANNEL1, user_id=user_id
         )
         member = await client.get_chat_member(
             chat_id=FORCE_SUB_CHANNEL2, user_id=user_id
         )
         member = await client.get_chat_member(
-            chat_id=FORCE_SUB_CHANNEL3, user_id=user_id
+            chat_id=FORCE_SUB_GROUP1, user_id=user_id
         )
         member = await client.get_chat_member(
-            chat_id=FORCE_SUB_CHANNEL4, user_id=user_id
+            chat_id=FORCE_SUB_GROUP2, user_id=user_id
         )
     except UserNotParticipant:
         return False
@@ -81,7 +81,7 @@ async def get_messages(client, message_ids):
             msgs = await client.get_messages(
                 chat_id=client.db_channel.id, message_ids=temb_ids
             )
-        except:
+        except BaseException:
             pass
         total_messages += len(temb_ids)
         messages.extend(msgs)
@@ -89,28 +89,25 @@ async def get_messages(client, message_ids):
 
 
 async def get_message_id(client, message):
-    if message.forward_from_chat:
-        if message.forward_from_chat.id == client.db_channel.id:
-            return message.forward_from_message_id
-        else:
-            return 0
-    elif message.forward_sender_name:
+    if (
+        message.forward_from_chat
+        and message.forward_from_chat.id == client.db_channel.id
+    ):
+        return message.forward_from_message_id
+    elif message.forward_from_chat or message.forward_sender_name or not message.text:
         return 0
-    elif message.text:
-        pattern = "https://t.me/(?:c/)?(.*)/(\d+)"
+    else:
+        pattern = "https://t.me/(?:c/)?(.*)/(\\d+)"
         matches = re.match(pattern, message.text)
         if not matches:
             return 0
-        channel_id = matches.group(3)
+        channel_id = matches.group(1)
         msg_id = int(matches.group(2))
         if channel_id.isdigit():
             if f"-100{channel_id}" == str(client.db_channel.id):
                 return msg_id
-        else:
-            if channel_id == client.db_channel.username:
-                return msg_id
-    else:
-        return 0
+        elif channel_id == client.db_channel.username:
+            return msg_id
 
 
 subscribed = filters.create(is_subscribed)
