f86046563f09399ec29e6a3e4acbfad16a10bed9
mrismanaziz <mrismanaziz@gmail.com>
2022-03-26 02:50:01 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import logging
 import os
+from dotenv import load_dotenv
 from logging.handlers import RotatingFileHandler
+
+load_dotenv("config.env")
 
 # Bot token @Botfather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
